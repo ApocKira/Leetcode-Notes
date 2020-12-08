e6843cc2cecf7ba92125731ce7f90cca7b29e7fa
Zhiwen Yang <yzw0225@gmail.com>
2020-12-08 12:13:12 -0500

    Notes added

--- Common/swap.java
+++ Common/swap.java
@@ -1,8 +1,8 @@
-public class SwapValues {
+public class swap {
     public static void main(String[] args){
     	int a,b,temp;
-	temp = a;
-	a = b;
-	b = temp;
+		temp = a;
+		a = b;
+		b = temp;
     }
 }
